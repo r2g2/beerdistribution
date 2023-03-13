4f46baf67255820bf8504b4f8cd0cad55a962f40
Rain Gong <rain_gong@cargill.com>
2023-03-13 08:23:21 +0800

    test for proxy

---
+++ test-client.js
@@ -0,0 +1,25 @@
+const socket = require('socket.io-client')('ws://localhost:3000');
+
+socket.on('connect', () => {
+  // either with send()
+  socket.send('Hello!');
+
+  // or with emit() and custom event names
+  socket.emit('salutations', 'Hello!', { 'mr': 'john' }, Uint8Array.from([1, 2, 3, 4]));
+});
+
+// handle the event sent with socket.send()
+socket.on('message', data => {
+  console.log(data);
+  //socket.send(data);
+});
+
+// handle the event sent with socket.emit()
+socket.on('greetings', (elem1, elem2, elem3) => {
+  console.log(elem1, elem2, elem3);
+});
+
+
+const items = ['item1', 'item2', 'item3'];
+
+items.forEach(item => { socket.send(item); });
